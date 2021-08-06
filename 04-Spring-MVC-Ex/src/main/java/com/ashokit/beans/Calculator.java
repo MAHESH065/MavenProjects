9f424d98894ebe6d830c98de3b08e7a634e74518
Mahesh <maheshwar.4ever@gmail.com>
2021-08-06 10:52:35 +0530

    cccc

--- 04-Spring-MVC-Ex/src/main/java/com/ashokit/beans/Calculator.java
+++ 04-Spring-MVC-Ex/src/main/java/com/ashokit/beans/Calculator.java
@@ -3,6 +3,6 @@ package com.ashokit.beans;
 public class Calculator {
 	public void add(int a, int b) {
 		int res = a+b;
-		System.out.println("Restult :: "+res);
+		System.out.println("Restult ::: "+res);
 	}	
 }
